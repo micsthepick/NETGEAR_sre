bd998b57c077a3f31eae9f4ec1a2ee3e3f340bc0
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-17 10:38:56 +1000

    fixup newline

--- compile_fw_hacks.sh
+++ compile_fw_hacks.sh
@@ -1,10 +1,17 @@
 rm -f fw_hacks.so
-export CFLAGS="-march=armv5te -mfloat-abi=soft -mfpu=vfp"
-export FW_HACKS_HOME=$(pwd)
-export MUSLHOME=$FW_HACKS_HOME/arm-unknown-linux-musleabi
-export gcc="$MUSLHOME/bin/arm-unknown-linux-musleabi-gcc"
+CFLAGS="-march=armv5te -mfloat-abi=soft -mfpu=vfp"
+FW_HACKS_HOME="$(pwd)"
+MUSLHOME="$FW_HACKS_HOME/arm-unknown-linux-musleabi"
+gcc="$MUSLHOME/bin/arm-unknown-linux-musleabi-gcc"
+echo '
 $gcc -g $CFLAGS -nostdlib \
   -nodefaultlibs \
   -isystem $MUSLHOME/arm-unknown-linux-musleabi/include \
   -L $MUSLHOME/arm-unknown-linux-musleabi/lib \
   $CFLAGS -Werror ./fw_hacks.c -fPIC -shared -o ./fw_hacks.so
+'
+
+echo $CFLAGS
+echo $FW_HACKS_HOME
+echo $MUSLHOME
+echo $gcc
