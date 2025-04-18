8ba29871aadd626f1ad444457de7eb2db02b497a
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-19 04:32:34 +1000

    improve scripts

--- cc.sh
+++ cc.sh
@@ -1,1 +1,5 @@
+if [ ! -e "./arm-unknown-linux-musleabi" ]
+then
+    curl -L https://github.com/musl-cross/musl-cross/releases/download/20250206/arm-unknown-linux-musleabi.tar.xz | tar xJ
+fi
 ./compile_fw_hacks.sh && sudo cp ./fw_hacks.so squashfs_root_bb/
--- run_args.sh
+++ run_args.sh
@@ -1,4 +1,4 @@
 systemctl start systemd-binfmt
 test -n "$QS" && export CHROOTENV="$CHROOTENV QEMU_STRACE="
 test -n "$QD" && export QEMUDBG=" qemu-arm-static -g 241 "
-chroot squashfs_root_bb /usr/bin/env -i HOME=/ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin "SHELL=/bin/sh" $CHROOTENV LD_PRELOAD=/fw_hacks.so $QEMUDBG $@
+chroot squashfs_root_bb /usr/bin/env -i HOME=/ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin "SHELL=/bin/sh" $CHROOTENV LD_PRELOAD=/fw_hacks.so  $QEMUDBG $@
