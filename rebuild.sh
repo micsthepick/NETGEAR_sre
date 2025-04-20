8de4daf901f91b36d7147a88d05ae9b7f9551092
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-20 18:16:15 +1000

    SCRIPTS: technically just a tar

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 if [ -e squashfs.tar.gz ]
 then
     sudo mkdir -p squashfs_root_bb
-    sudo tar xf squashfs.tar.gz -C squashfs_root_bb
+    sudo tar xf squashfs.tar -C squashfs_root_bb
     sudo ./mounts_for_fw_pack.sh
     sudo ./first_startup.sh
     sudo ./cc.sh
