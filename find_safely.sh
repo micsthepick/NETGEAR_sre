0f2082281683fbfdf27f5d2942367b5385d8e4b3
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-21 20:27:14 +1000

    scripts: change find_safely again

--- find_safely.sh
+++ find_safely.sh
@@ -1,6 +1,1 @@
-if [ "$1" = "" ]
-then
-    echo "please provide a search pattern."
-    exit 1
-fi
-grep --color=auto -r --exclude-dir=proc --exclude-dir=run "$1" squashfs_root_bb $(@:2)
+(cd squashfs_root_bb && grep --color=auto -r --exclude-dir=proc --exclude-dir=run "$@")
