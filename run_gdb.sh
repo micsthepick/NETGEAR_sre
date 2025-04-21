9e13f0c25bcc9a7360a307387c3561a1fcaef9ec
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-22 09:50:21 +1000

    better debugging experience

--- run_gdb.sh
+++ run_gdb.sh
@@ -1,1 +1,1 @@
-gdb-multiarch -ex "set sysroot $(pwd)/squashfs_root_bb/" -ex "target remote :241"
+gdb-multiarch -ex "set sysroot $(pwd)/squashfs_root_bb/" -ex "target remote :241" -ex "file squashfs_root_bb/fw_hacks.so"
---
+++ run_gef.sh
@@ -0,0 +1,1 @@
+gdb-multiarch -nx -ex "source gef.py" -ex "set sysroot $(pwd)/squashfs_root_bb/" -ex "gef-remote --qemu-user --qemu-binary squashfs_root_bb/usr/bin/env localhost 241" -ex "file squashfs_root_bb/fw_hacks.so"
