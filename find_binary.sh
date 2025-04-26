a27a7b507019e128b9758c6a6ce1fe1453717ea9
micsthepick <11528421+micsthepick@users.noreply.github.com>
2025-04-26 20:21:17 +1000

    better find_binary.sh script

--- find_binary.sh
+++ find_binary.sh
@@ -1,1 +1,1 @@
-find squashfs_root_bb -type f -exec sh -c 'xxd -p "$0" | tr -d "\n" |  grep -q "$@" && echo "$0"' {} $@ \;
+find squashfs_root_bb \( -path "squashfs_root_bb/proc" -o -path "squashfs_root_bb/run" \) -prune -o -type f -exec sh -c 'xxd -p "$0" | tr -d "\n" | grep -q "$@" && echo "$0"' {} $@ \;
